d0f24ccc821921fb7bdd8f25cd9fc454b69cee3f
Csongor <brcsongor@gmail.com>
2021-12-07 12:46:42 -0100

    The code is working, but still needs fine-tuning.

--- arithmetic.py
+++ arithmetic.py
@@ -1,27 +1,22 @@
-def n_term():
-    n = int(input("Which element do you need?\t"))
-    nterm = first + ((n - 1) * difference)
+def n_term(n,first,difference):
+    nterm = first+((n-1)*difference)
+    print(nterm)
     return nterm
-def sum_of_n():
-    n = int(input("How many elements do you need summarised?\t"))
-    sum = (n*(first + nterm()))/2
+def sum_of_n(n,first,nterm):
+    sum = (n*(first+nterm))/2
+    print(sum)
     return sum
-def pyramid():
-    n = int(input("How many levels should the pyramid have?\t"))
-    piramid = n*(first + nterm())
+def pyramid(n,nterm):
+    piramid = n*(first+nterm)
+    print(piramid)
+    return piramid
 while True:
     first = int(input("What should be the first/starting component?\t"))
     difference = int(input("How much should the difference be?\t"))
     need = str(input("What do you need? (nterm, sum, or pyramid)\t"))
-    nterm = 0
-    sum = 0
-    piramid = 0
     if need == "nterm":
-        n_term()
-        print(nterm)
+        n_term(int(input("Which element dou you need?\t")),first,difference)
     elif need == "sum":
-        sum_of_n()
-        print(sum)
+        sum_of_n(int(input("How many elements you need summarised?\t")),first,n_term(int(input("Which element dou you need?\t")),first,difference))
     else:
-        pyramid()
-        print(piramid)
+        pyramid(int(input("How many levels shoud the prymaid have?\t")),n_term(int(input("Which element dou you need?\t")),first,difference))
